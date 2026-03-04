010337b3c83049ab03f2f4bd6c702ad4d401d588
dsa069 <dsa069@inlumine.ual.es>
2026-03-04 16:43:53 +0100

    refactorizando proyecto siguiendo el tuto en vez de claude que hizo de mas y no meha gustado

--- src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getIndex, getAbout } from '../controllers/indexController';
-
-const router = Router();
-
-router.get('/', getIndex);
-router.get('/about', getAbout);
-
-export default router;

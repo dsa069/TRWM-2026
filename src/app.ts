bdd6c55787688506b858bde3ac53e63a52adbba2
dsa069 <dsa069@inlumine.ual.es>
2026-03-07 16:48:28 +0000

    Boostrap y dependencias de inicio devcontainer

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,30 @@
-import createError from 'http-errors';
-import express, { Request, Response, NextFunction } from 'express';
-import path from 'path';
-import cookieParser from 'cookie-parser';
-import logger from 'morgan';
+import createError from "http-errors";
+import express, { Request, Response, NextFunction } from "express";
+import path from "path";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
 
-import indexRouter from './routes/index';
-import usersRouter from './routes/users';
+import indexRouter from "./routes/index";
+import usersRouter from "./routes/users";
 
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, '..', 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "..", "views"));
+app.set("view engine", "pug");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(express.static(path.join(__dirname, "..", "public")));
+app.use(
+  "/bootstrap",
+  express.static(path.join(__dirname, "..", "node_modules/bootstrap/dist")),
+);
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use((_req: Request, _res: Response, next: NextFunction) => {
@@ -30,10 +34,10 @@ app.use((_req: Request, _res: Response, next: NextFunction) => {
 // error handler
 app.use((err: any, req: Request, res: Response, _next: NextFunction) => {
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 export default app;

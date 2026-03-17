7e0b0cdadb374d319871d89eee2c3d95d256dbf4
dsa069 <dsa069@inlumine.ual.es>
2026-03-17 18:50:39 +0000

    avances

--- app_server/controllers/location.ts
+++ app_server/controllers/location.ts
@@ -2,15 +2,36 @@ import { Request, Response } from "express";
 
 const homeList = (req: Request, res: Response): void => {
   res.render("locations-list", {
-    title: "Home",
+    title: "Locator - find a place to work with wifi",
+    pageHeader: {
+      title: "Loc8r",
+      strapline: "Find places to work with wifi near you!",
+    },
+    locations: [
+      {
+        name: "Starcups",
+        address: "125 High Street, Reading, RG6 1PS",
+        rating: 3,
+        facilities: ["Hot drinks", "Food", "Premium wifi"],
+        distance: "100m",
+      },
+      {
+        name: "Cafe Hero",
+        address: "125 High Street, Reading, RG6 1PS",
+        rating: 4,
+        facilities: ["Hot drinks", "Food", "Premium wifi"],
+        distance: "200m",
+      },
+      {
+        name: "Burger Queen",
+        address: "125 High Street, Reading, RG6 1PS",
+        rating: 2,
+        facilities: ["Food", "Premium wifi"],
+        distance: "250m",
+      },
+    ],
   });
 };
-
-/*const homeList = (req: Request, res: Response): void => {
-  res.render("index", {
-    title: "Home",
-  });
-};*/
 
 const locationInfo = (req: Request, res: Response): void => {
   res.render("locations-info", {
@@ -18,14 +39,8 @@ const locationInfo = (req: Request, res: Response): void => {
   });
 };
 
-/*const locationInfo = (req: Request, res: Response): void => {
-  res.render("index", {
-    title: "Location Info",
-  });
-};*/
-
 const addReview = (req: Request, res: Response): void => {
-  res.render("index", {
+  res.render("location-review-form", {
     title: "Add Review",
   });
 };

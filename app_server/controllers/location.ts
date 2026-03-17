ced47645de28b610372a4515f8160868a2d0e6b6
dsa069 <dsa069@inlumine.ual.es>
2026-03-17 18:10:18 +0000

    primera aproximacion locations-info

--- app_server/controllers/location.ts
+++ app_server/controllers/location.ts
@@ -1,16 +1,28 @@
 import { Request, Response } from "express";
 
 const homeList = (req: Request, res: Response): void => {
-  res.render("index", {
+  res.render("locations-list", {
     title: "Home",
   });
 };
 
-const locationInfo = (req: Request, res: Response): void => {
+/*const homeList = (req: Request, res: Response): void => {
   res.render("index", {
+    title: "Home",
+  });
+};*/
+
+const locationInfo = (req: Request, res: Response): void => {
+  res.render("locations-info", {
     title: "Location Info",
   });
 };
+
+/*const locationInfo = (req: Request, res: Response): void => {
+  res.render("index", {
+    title: "Location Info",
+  });
+};*/
 
 const addReview = (req: Request, res: Response): void => {
   res.render("index", {

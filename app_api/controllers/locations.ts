a982cdc0974800f2be7cf6aa178d9784686eff87
dsa069 <dsa069@inlumine.ual.es>
2026-04-14 18:51:09 +0200

    Enhance locationsUpdate and locationsDelete error handling and response messages

--- app_api/controllers/locations.ts
+++ app_api/controllers/locations.ts
@@ -32,3 +32,37 @@ export const locationsCreate = async (req: Request, res: Response) => {
     res.status(400).json({ error: err });
   }
 };
+
+export const locationsUpdate = async (req: Request, res: Response) => {
+  try {
+    const location = await Location.findByIdAndUpdate(
+      req.params.locationId,
+      req.body,
+      //New es para devolver el documento actualizado, en lugar del antiguo.
+      //Que se validen los datos antes de actualizar
+      { new: true, runValidators: true }
+    ).exec();
+    if (!location) return res.status(404).json({ message: "not found" });
+    res.status(200).json(location);
+  } catch (err: any) {
+    console.error(err.message);
+    if (err.name === "CastError") {
+      return res.status(400).json({ message: "Bad Request" });
+    }
+    res.status(500).json({ message: "Unknown Error" });
+  }
+};
+
+export const locationsDelete = async (req: Request, res: Response) => {
+  try {
+    const location = await Location.findByIdAndDelete(req.params.locationId).exec();
+    if (!location) return res.status(404).json({ message: "not found" });
+    return res.status(204).json();
+  } catch (err: any) {
+    console.error(err.message);
+    if (err.name === "CastError") {
+      return res.status(400).json({ message: "Bad Request" });
+    }
+    res.status(500).json({ message: "Unknown Error" });
+  }
+};
